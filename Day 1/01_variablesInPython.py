0e653ccf3fda5a397c1d83ba5bb0338b96795141
Harshit Mishra <harshitm312@gmail.com>
2026-01-22 11:44:23 +0530

    Day 1 Commit

---
+++ Day 1/01_variablesInPython.py
@@ -0,0 +1,17 @@
+x = 5
+y = 10
+print(x)
+print(y)
+print(x+y)
+name = "HelloWorld"
+print(name)
+print(name + 'rocks')
+print(name[1])
+print(name[-1])
+print(name[0:2])
+print(name[1:4])
+print(name[1:])
+print(name[:4])
+print(name[3:10])
+name[0] = 'a'
+print(name)
