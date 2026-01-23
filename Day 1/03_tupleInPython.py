6053ee5274bdd4dfe9ca2855eab5329abe09a097
Harshit Mishra <harshitm312@gmail.com>
2026-01-23 13:21:11 +0530

    Adding set and tuples

---
+++ Day 1/03_tupleInPython.py
@@ -0,0 +1,16 @@
+# Tupple in python
+tup = (21,4,5,34)
+print(tup)
+
+# Fetching the tuple values
+print(tup[1])
+
+# Tuple are immutable, it will throw an error
+# tup[1] = 30
+
+# Count will count the number of values present in the tuple
+print(tup.count)
+
+
+
+
---
+++ Day 1/04_setInPython.py
@@ -0,0 +1,12 @@
+# Set in python is used to store only unique values
+s1 = {22,25,14,21,5}
+print(s1)
+
+#  Set don't allow the duplication and also its sequence is not predicted, it use hash set to fetch value faster
+s2 = {20,21,23,27,21}
+print(s2)
+
+# As the values are put randomly so we can't use indexing in set
+# print(s1[2])
+
+
