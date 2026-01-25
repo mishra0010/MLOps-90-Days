87d65577ec1e22d9e2920c2d2921b11b6571775e
Harshit Mishra <harshitm312@gmail.com>
2026-01-25 16:43:44 +0530

    numpy

---
+++ Day 4/numpyInPython.py
@@ -0,0 +1,26 @@
+import numpy as np
+list1 = [1,2,3,4,5]
+#print(list1)
+#print(list1[0])
+
+list2 = ["Jhon elder",41,list1,True]
+#print(list2)
+
+# Numpy - Numeric python
+# ndarray = n-dimensional array
+
+np1 = np.array([0,1,2,3,4,5,6,7,8,9])
+print(np1)
+print(np1.shape)
+
+np2 = np.arange(10)
+print(np2)
+
+np3 = np.arange(0,10,2)
+print(np3)
+
+np4 = np.zeros(10)
+print(np4)
+
+np5 = np.zeros((2,10))
+print(np5)
